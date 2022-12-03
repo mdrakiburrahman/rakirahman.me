50a018c9967e690eb61825b4dfafce0191274f27
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2022-12-03 12:18:25 -0500

    Test node version

---
+++ .devcontainer/scripts/post-attach-commands.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+
+# Start server
+gatsby develop
---
+++ .devcontainer/scripts/post-create-commands.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -xe
+
+# Fix workspace permissions
+sudo chown -R vscode:sudo /workspaces
+
+# Create directory for node version manager
+sudo mkdir -p /usr/local/share/nvm
+sudo chown -R vscode:sudo /usr/local/share/nvm
+
+# Node Packages
+npm install -g gatsby-cli@2.12.21
+npm install
+
+git clean -fdx
